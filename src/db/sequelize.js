f72e22f9e7ad466b2966284525a4885aa6825cd4
IHOR <i.mukhametov@student.bachelor-goit.com.ua>
2026-07-05 01:17:32 +0300

    Fix PostgreSQL Sequelize configuration

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -7,12 +7,13 @@ export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
   logging: false,
-  dialectOptions: {
-    ssl: process.env.NODE_ENV === "production"
+  dialectOptions:
+    process.env.NODE_ENV === "production"
       ? {
-          require: true,
-          rejectUnauthorized: false,
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
         }
-      : false,
-  },
+      : {},
 });
